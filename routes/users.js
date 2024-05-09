396cd38fb0a2f6f0d5fd9ece9df6097a84562413
Béa <beatrice.becco@gmail.com>
2024-05-09 18:27:32 +0200

    Add routes get/users, signin, signup

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
 
+require('../models/connection');
+const User = require('../models/users');
+const { checkBody } = require('../modules/ckeckBody')
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res) => {
+  User.find()
+    .then(data => {
+      res.json({ users: data })
+    })
+})
+
+//ADD A NEW USER IN DB
+router.post('/signup', (req, res) => {
+  if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  User.findOne({ username: req.body.username })
+    .then(data => {
+      if (data) {
+        res.json({ result: false, error: 'User already exists' })
+        return
+      } else {
+        const newUser = new User({
+          username: req.body.username,
+          password: req.body.password
+        })
+        newUser.save().then(newDoc => {
+          res.json({ newUser: newDoc })
+        })
+      }
+    })
 });
+//CONNECT A REGISTERED USER
+router.post('/signin', (req, res) => {
+  if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+  User.findOne({ username: req.body.username, password: req.body.password })
+    .then(data => {
+      if (data) {
+        res.json({ result: true })
+      } else {
+        res.json({ result: false, error: 'User doesn\'t exists' })
+      }
+    })
+});
+
 
 module.exports = router;
